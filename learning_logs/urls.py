9ee0d6fbebea804be614cf3c627b4e50d2e8ac8d
bellahuang <bellahuang1012@gmail.com>
2017-12-02 18:24:37 -0500

    add topic html

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
 
 	# show all topics
 	url(r'^topics/$',views.topics, name='topics'),
+
+	# show all information within a specific topic
+	url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
