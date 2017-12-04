44e1bdedd84f90f3e444f2e05fc5438c92407499
bellahuang <bellahuang1012@gmail.com>
2017-12-04 18:50:52 -0500

    add foreign key User to Topic model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # create a class Topic inherit from Model
 class Topic(models.Model):
 	"""the topic of user learning"""
 	text = models.CharField(max_length=200)
 	date_added = models.DateTimeField(auto_now_add=True)
+	owner = models.ForeignKey(User)
 
 	def __str__(self):
 		return self.text
